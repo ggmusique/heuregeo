8de2ae94a4752fad85c0efe0a57cb0f8c0cceb11
ggmusique <geohelene@msn.com>
2026-02-24 18:29:13 +0100

    ok

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED. Do not edit by hand.
 export const BUILD_INFO = {
-  branch: "--abbrev-ref\n22755dc709ea3b85d26ec7c26231a14af97429ee",
-  commit: "--short\n22755dc709ea3b85d26ec7c26231a14af97429ee",
-  date: "2026-02-24T17:26:14.214Z",
+  branch: "--abbrev-ref\ne5f76cb64a9dd5ccad25308e33f6862de6f791a5",
+  commit: "--short\ne5f76cb64a9dd5ccad25308e33f6862de6f791a5",
+  date: "2026-02-24T17:28:11.205Z",
 };
