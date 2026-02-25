d9c2443bd693994bdbbedb1a8ba5ed07c00cbe70
geo <53321512-geohelene@users.noreply.replit.com>
2026-02-25 17:10:43 +0000

    chore: trigger rebuild with env vars

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED. Do not edit by hand.
 export const BUILD_INFO = {
-  branch: "--abbrev-ref\n9ea97ff9d02da2ed54f4cad52fd8ad973aa3f6c6",
-  commit: "--short\n9ea97ff9d02da2ed54f4cad52fd8ad973aa3f6c6",
-  date: "2026-02-24T17:41:41.367Z",
+  branch: "work",
+  commit: "645e08c",
+  date: "2026-02-25T16:58:21.237Z",
 };
