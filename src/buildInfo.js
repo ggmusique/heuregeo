f2fd0b31a6f7035f905889534be33a7e4ebf9e5d
Geo91033414 <geohelene@msn.com>
2026-02-26 10:27:17 +0100

    refonte: first working local setup

--- src/buildInfo.js
+++ src/buildInfo.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED. Do not edit by hand.
 export const BUILD_INFO = {
-  branch: "work",
-  commit: "645e08c",
-  date: "2026-02-25T16:58:21.237Z",
+  branch: "refonte",
+  commit: "cc58291",
+  date: "2026-02-26T09:25:05.437Z",
 };
