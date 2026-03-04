ed76592ad788f1695824ad1625681f65af3df9e6
ggmusique <geohelene@msn.com>
2026-03-04 16:08:25 +0000

    packa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-50fc7337'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hms375sqpoo"
+    "revision": "0.c2998bmm5lc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
