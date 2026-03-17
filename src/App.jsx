46ee526390b7bcb546c9b580422e1205b1a4f2ae
Geo91033414 <geohelene@msn.com>
2026-03-17 15:14:12 +0100

    fix: remove duplicate props in MainLayout usage

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ export default function App({ user }) {
     <LabelsContext.Provider value={labels}>
       <ThemeContext.Provider value={themeProvider}>
         {/* NOUVEAU: Utilisation du MainLayout */}
-        <MainLayout isPro={isPro} isViewer={isViewer} APP_VERSION={APP_VERSION} isPro={isPro} isViewer={isViewer} APP_VERSION={APP_VERSION} 
+        <MainLayout isPro={isPro} isViewer={isViewer} APP_VERSION={APP_VERSION} 
           profile={profile} 
           themeConfig={themeConfig} 
           isDark={isDark} 
@@ -241,5 +241,6 @@ export default function App({ user }) {
     </LabelsContext.Provider>
   );
 }
+
 
 
